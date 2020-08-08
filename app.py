d1b87267dd9335c2678dab1c616aa2dedd5bcb19
SovSauce <sovsauce@gmail.com>
2020-08-08 21:38:22 +0000

    all asia countries: 50

--- app.py
+++ app.py
@@ -18,119 +18,269 @@ def index():
     return render_template('index.html', time=datetime.now())
 
 
-@app.route('/asians')
-def asians():
-    return render_template('asians.html', time=datetime.now())
+@app.route('/asia')
+def asia():
+    return render_template('asia.html', time=datetime.now())
 
 
-@app.route('/asians/chinese')
-def asianschinese():
-    return render_template('asians/chinese.html', time=datetime.now())
+@app.route('/asia/afganistan')
+def asiaafganistan():
+    return render_template('asia/afganistan.html', time=datetime.now())
 
 
-@app.route('/asians/japanese')
-def asiansjapanese():
-    return render_template('asians/japanese.html', time=datetime.now())
+@app.route('/asia/armenia')
+def asiaarmenia():
+    return render_template('asia/armenia.html', time=datetime.now())
 
 
-@app.route('/asians/korean')
-def asianskorean():
-    return render_template('asians/korean.html', time=datetime.now())
+@app.route('/asia/azerbaijan')
+def asiaazerbaijan():
+    return render_template('asia/azerbaijan.html', time=datetime.now())
 
 
-@app.route('/asians/malaysian')
-def asiansmalaysian():
-    return render_template('asians/malaysian.html', time=datetime.now())
+@app.route('/asia/bahrain')
+def asiabahrain():
+    return render_template('asia/bahrain.html', time=datetime.now())
 
 
-@app.route('/asians/indian')
-def asiansindian():
-    return render_template('asians/indian.html', time=datetime.now())
+@app.route('/asia/bangladesh')
+def asiabangladesh():
+    return render_template('asia/bangladesh.html', time=datetime.now())
 
 
-@app.route('/asians/indonesian')
-def asiansindonesian():
-    return render_template('asians/indonesian.html', time=datetime.now())
+@app.route('/asia/bhutan')
+def asiabhutan():
+    return render_template('asia/bhutan.html', time=datetime.now())
 
 
-@app.route('/asians/bangladeshis')
-def asiansbangladeshis():
-    return render_template('asians/bangladeshis.html', time=datetime.now())
+@app.route('/asia/brunei')
+def asiabrunei():
+    return render_template('asia/brunei.html', time=datetime.now())
 
 
-@app.route('/asians/thais')
-def asiansthais():
-    return render_template('asians/thais.html', time=datetime.now())
+@app.route('/asia/burma')
+def asiaburma():
+    return render_template('asia/burma.html', time=datetime.now())
 
 
-@app.route('/asians/vietnamese')
-def asiansvietnamese():
-    return render_template('asians/vietnamese.html', time=datetime.now())
+@app.route('/asia/cambodia')
+def asiacambodia():
+    return render_template('asia/cambodia.html', time=datetime.now())
 
 
-@app.route('/asians/hongkongers')
-def asianshongkongers():
-    return render_template('asians/hongkongers.html', time=datetime.now())
+@app.route('/asia/china')
+def asiachin():
+    return render_template('asia/china.html', time=datetime.now())
 
 
-@app.route('/asians/myanmars')
-def asiansmyanmars():
-    return render_template('asians/myanmars.html', time=datetime.now())
+@app.route('/asia/cyprus')
+def asiacyprus():
+    return render_template('asia/cyprus.html', time=datetime.now())
 
 
-@app.route('/asians/laotians')
-def asianslaotians():
-    return render_template('asians/laotians.html', time=datetime.now())
+@app.route('/asia/eastTimor')
+def asiaeastTimor():
+    return render_template('asia/eastTimor.html', time=datetime.now())
 
 
-@app.route('/asians/israelis')
-def asiansisraelis():
-    return render_template('asians/israelis.html', time=datetime.now())
+@app.route('/asia/georgia')
+def asiageorgia():
+    return render_template('asia/georgia.html', time=datetime.now())
 
 
-@app.route('/asians/iranians')
-def asiansiranians():
-    return render_template('asians/iranians.html', time=datetime.now())
+@app.route('/asia/hongkong')
+def asiahongkong():
+    return render_template('asia/hongkong.html', time=datetime.now())
 
 
-@app.route('/asians/taiwanese')
-def asianstaiwanese():
-    return render_template('asians/taiwanese.html', time=datetime.now())
+@app.route('/asia/india')
+def asiaindia():
+    return render_template('asia/india.html', time=datetime.now())
 
 
-@app.route('/asians/saudiArabian')
-def asianssaudiArabian():
-    return render_template('asians/saudiArabian.html', time=datetime.now())
+@app.route('/asia/indonesia')
+def asiaindonesia():
+    return render_template('asia/indonesia.html', time=datetime.now())
 
 
-@app.route('/asians/mongolian')
-def asiansmongolian():
-    return render_template('asians/mongolian.html', time=datetime.now())
+@app.route('/asia/iran')
+def asiairan():
+    return render_template('asia/iran.html', time=datetime.now())
 
 
-@app.route('/asians/bruneian')
-def asiansbruneian():
-    return render_template('asians/bruneian.html', time=datetime.now())
+@app.route('/asia/iraq')
+def asiairaq():
+    return render_template('asia/iraq.html', time=datetime.now())
 
 
-@app.route('/asians/nepal')
-def asiansnepal():
-    return render_template('asians/nepal.html', time=datetime.now())
+@app.route('/asia/israel')
+def asiaisrael():
+    return render_template('asia/israel.html', time=datetime.now())
 
 
-@app.route('/asians/afganistan')
-def asiansafganistan():
-    return render_template('asians/afganistan.html', time=datetime.now())
+@app.route('/asia/japan')
+def asiajapan():
+    return render_template('asia/japan.html', time=datetime.now())
 
 
-@app.route('/asians/sriLanka')
-def asianssriLanka():
-    return render_template('asians/sriLanka.html', time=datetime.now())
+@app.route('/asia/jordan')
+def asiajordan():
+    return render_template('asia/jordan.html', time=datetime.now())
 
 
-@app.route('/americanIndian')
-def americanIndian():
-    return render_template('americanIndian.html', time=datetime.now())
+@app.route('/asia/kazakhstan')
+def asiakazakhstan():
+    return render_template('asia/kahzakhstan.html', time=datetime.now())
+
+
+@app.route('/asia/kuwait')
+def asiakuwait():
+    return render_template('asia/kuwait.html', time=datetime.now())
+
+
+@app.route('/asia/kyrgyzstan')
+def asiakyrgyzstan():
+    return render_template('asia/kyrgyzstan.html', time=datetime.now())
+
+
+@app.route('/asia/laos')
+def asialaos():
+    return render_template('asia/laos.html', time=datetime.now())
+
+
+@app.route('/asia/lebanon')
+def asialebanon():
+    return render_template('asia/lebanon.html', time=datetime.now())
+
+
+@app.route('/asia/malaysia')
+def asiamalaysia():
+    return render_template('asia/malaysia.html', time=datetime.now())
+
+
+@app.route('/asia/myanmar')
+def asiamyanmar():
+    return render_template('asia/myanmar.html', time=datetime.now())
+
+
+@app.route('/asia/mongolia')
+def asiamongolia():
+    return render_template('asia/mongolia.html', time=datetime.now())
+
+
+@app.route('/asia/nepal')
+def asianepal():
+    return render_template('asia/nepal.html', time=datetime.now())
+
+
+@app.route('/asia/northKorea')
+def asiakorea():
+    return render_template('asia/korea.html', time=datetime.now())
+
+
+@app.route('/asia/oman')
+def asiaoman():
+    return render_template('asia/oman.html', time=datetime.now())
+
+
+@app.route('/asia/pakistan')
+def asiapakistan():
+    return render_template('asia/pakistan.html', time=datetime.now())
+
+
+@app.route('/asia/papuaNewGuinea')
+def asiapapuaNewGuinea():
+    return render_template('asia/papuaNewGuinea.html', time=datetime.now())
+
+
+@app.route('/asia/philippines')
+def asiaphilippines():
+    return render_template('asia/philippines.html', time=datetime.now())
+
+
+@app.route('/asia/qatar')
+def asiaqatar():
+    return render_template('asia/qatar.html', time=datetime.now())
+
+
+@app.route('/asia/russia')
+def asiarussia():
+    return render_template('asia/russia.html', time=datetime.now())
+
+
+@app.route('/asia/saudiArabia')
+def asiasaudiArabia():
+    return render_template('asia/saudiArabia.html', time=datetime.now())
+
+
+@app.route('/asia/singapore')
+def asiasingapore():
+    return render_template('asia/singapore.html', time=datetime.now())
+
+
+@app.route('/asia/southKorea')
+def asiasouthKorea():
+    return render_template('asia/southKorea.html', time=datetime.now())
+
+
+@app.route('/asia/sriLanka')
+def asiasriLanka():
+    return render_template('asia/sriLanka.html', time=datetime.now())
+
+
+@app.route('/asia/syria')
+def asiasyria():
+    return render_template('asia/syria.html', time=datetime.now())
+
+
+@app.route('/asia/taiwan')
+def asiataiwan():
+    return render_template('asia/taiwan.html', time=datetime.now())
+
+
+@app.route('/asia/tajikstan')
+def asiatajikstan():
+    return render_template('asia/tajikstan.html', time=datetime.now())
+
+
+@app.route('/asia/thailand')
+def asiathailand():
+    return render_template('asia/thailand.html', time=datetime.now())
+
+
+@app.route('/asia/turkey')
+def asiaturkey():
+    return render_template('asia/turkey.html', time=datetime.now())
+
+
+@app.route('/asia/turkmenistan')
+def asiaturkmenistan():
+    return render_template('asia/turkmenistan.html', time=datetime.now())
+
+
+@app.route('/asia/unitedArabEmirates')
+def asiaunitedArabEmirates():
+    return render_template('asia/unitedArabEmirates.html', time=datetime.now())
+
+
+@app.route('/asia/uzbekistan')
+def asiauzbekistan():
+    return render_template('asia/uzbekistan.html', time=datetime.now())
+
+
+@app.route('/asia/vietnam')
+def asiavietnam():
+    return render_template('asia/vietnam.html', time=datetime.now())
+
+
+@app.route('/asia/yemen')
+def asiayemen():
+    return render_template('asia/yemen.html', time=datetime.now())
+
+
+@app.route('/americanIndia')
+def americanIndia():
+    return render_template('americanIndia.html', time=datetime.now())
 
 
 @app.route('/pacificIslander')

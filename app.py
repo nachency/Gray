072ba20952486748b1cbfc957ab5fa937b791c62
SovSauce <sovsauce@gmail.com>
2020-08-13 02:24:36 +0000

    html

--- app.py
+++ app.py
@@ -25,80 +25,80 @@ def asia():
 
 @app.route('/asia/afganistan')
 def asiaafganistan():
-    return render_template('asia/afganistan.html', time=datetime.now())
+    return render_template('asiaafganistan.html', time=datetime.now())
 
 
 @app.route('/asia/armenia')
 def asiaarmenia():
-    return render_template('asia/armenia.html', time=datetime.now())
+    return render_template('asiaarmenia.html', time=datetime.now())
 
 
 @app.route('/asia/azerbaijan')
 def asiaazerbaijan():
-    return render_template('asia/azerbaijan.html', time=datetime.now())
+    return render_template('asiaazerbaijan.html', time=datetime.now())
 
 
 @app.route('/asia/bahrain')
 def asiabahrain():
-    return render_template('asia/bahrain.html', time=datetime.now())
+    return render_template('asiabahrain.html', time=datetime.now())
 
 
 @app.route('/asia/bangladesh')
 def asiabangladesh():
-    return render_template('asia/bangladesh.html', time=datetime.now())
+    return render_template('asiabangladesh.html', time=datetime.now())
 
 
 @app.route('/asia/bhutan')
 def asiabhutan():
-    return render_template('asia/bhutan.html', time=datetime.now())
+    return render_template('asiabhutan.html', time=datetime.now())
 
 
 @app.route('/asia/brunei')
 def asiabrunei():
-    return render_template('asia/brunei.html', time=datetime.now())
+    return render_template('asiabrunei.html', time=datetime.now())
 
 
 @app.route('/asia/burma')
 def asiaburma():
-    return render_template('asia/burma.html', time=datetime.now())
+    return render_template('asiaburma.html', time=datetime.now())
 
 
 @app.route('/asia/cambodia')
 def asiacambodia():
-    return render_template('asia/cambodia.html', time=datetime.now())
+    return render_template('asiacambodia.html', time=datetime.now())
 
 
 @app.route('/asia/china')
 def asiachin():
-    return render_template('asia/china.html', time=datetime.now())
+    return render_template('asiachina.html', time=datetime.now())
 
 
 @app.route('/asia/cyprus')
 def asiacyprus():
-    return render_template('asia/cyprus.html', time=datetime.now())
+    return render_template('asiacyprus.html', time=datetime.now())
 
 
 @app.route('/asia/eastTimor')
 def asiaeastTimor():
-    return render_template('asia/eastTimor.html', time=datetime.now())
+    return render_template('asiaeastTimor.html', time=datetime.now())
 
 
 @app.route('/asia/georgia')
 def asiageorgia():
-    return render_template('asia/georgia.html', time=datetime.now())
+    return render_template('asiageorgia.html', time=datetime.now())
 
 
 @app.route('/asia/india')
 def asiaindia():
-    return render_template('asia/india.html', time=datetime.now())
+    return render_template('asiaindia.html', time=datetime.now())
 
 
-@app.route('/asia/indonesia')
+@app.route('/asiaindonesia')
 def asiaindonesia():
     return render_template('asia/indonesia.html', time=datetime.now())
 
 
-@app.route('/asia/iran')
+@app.route('/asiairan')
 def asiairan():
     return render_template('asia/iran.html', time=datetime.now())
 
@@ -284,266 +284,266 @@ def asiamacao():
     return render_template('asia/macao.html', time=datetime.now())
 
 
-# LATIN AMERICA
-@app.route('/latinAmerica')
-def latinAmerica():
-    return render_template('latinAmerica.html', time=datetime.now())
+# # LATIN AMERICA
+# @app.route('/latinAmerica')
+# def latinAmerica():
+#     return render_template('latinAmerica.html', time=datetime.now())
 
 
-@app.route('/latinAmerica/antiguaAndBarbuda')
-def latinAmericaantiguaAndBarbuda():
-    return render_template('latinAmerica/antiguaAndBarbuda.html', time=datetime.now())
+# @app.route('/latinAmerica/antiguaAndBarbuda')
+# def latinAmericaantiguaAndBarbuda():
+#     return render_template('latinAmerica/antiguaAndBarbuda.html', time=datetime.now())
 
 
-@app.route('/latinAmerica/argentina')
-def latinAmericaargentina():
-    return render_template('latinAmerica/argentina.html', time=datetime.now())
+# @app.route('/latinAmerica/argentina')
+# def latinAmericaargentina():
+#     return render_template('latinAmerica/argentina.html', time=datetime.now())
 
 
-@app.route('/latinAmerica/bahamas')
-def latinAmericabahamas():
-    return render_template('latinAmerica/bahamas.html', time=datetime.now())
+# @app.route('/latinAmerica/bahamas')
+# def latinAmericabahamas():
+#     return render_template('latinAmerica/bahamas.html', time=datetime.now())
 
 
-@app.route('/latinAmerica/barbados')
-def latinAmericabarbados():
-    return render_template('latinAmerica/barbados.html', time=datetime.now())
+# @app.route('/latinAmerica/barbados')
+# def latinAmericabarbados():
+#     return render_template('latinAmerica/barbados.html', time=datetime.now())
 
 
-@app.route('/latinAmerica/belize')
-def latinAmericabelize():
-    return render_template('latinAmerica/belize.html', time=datetime.now())
+# @app.route('/latinAmerica/belize')
+# def latinAmericabelize():
+#     return render_template('latinAmerica/belize.html', time=datetime.now())
 
 
-@app.route('/latinAmerica/bolivia')
-def latinAmericabolivia():
-    return render_template('latinAmerica/bolivia.html', time=datetime.now())
+# @app.route('/latinAmerica/bolivia')
+# def latinAmericabolivia():
+#     return render_template('latinAmerica/bolivia.html', time=datetime.now())
 
 
-@app.route('/latinAmerica/brazil')
-def latinAmericabrazil():
-    return render_template('latinAmerica/brazil.html', time=datetime.now())
+# @app.route('/latinAmerica/brazil')
+# def latinAmericabrazil():
+#     return render_template('latinAmerica/brazil.html', time=datetime.now())
 
 
-@app.route('/latinAmerica/chile')
-def latinAmericachile():
-    return render_template('latinAmerica/chile.html', time=datetime.now())
+# @app.route('/latinAmerica/chile')
+# def latinAmericachile():
+#     return render_template('latinAmerica/chile.html', time=datetime.now())
 
 
-@app.route('/latinAmerica/colombia')
-def latinAmericacolombia():
-    return render_template('latinAmerica/colombia.html', time=datetime.now())
+# @app.route('/latinAmerica/colombia')
+# def latinAmericacolombia():
+#     return render_template('latinAmerica/colombia.html', time=datetime.now())
 
 
-@app.route('/latinAmerica/costaRica')
-def latinAmericacostaRica():
-    return render_template('latinAmerica/costaRica.html', time=datetime.now())
+# @app.route('/latinAmerica/costaRica')
+# def latinAmericacostaRica():
+#     return render_template('latinAmerica/costaRica.html', time=datetime.now())
 
 
-@app.route('/latinAmerica/cuba')
-def latinAmericacuba():
-    return render_template('latinAmerica/cuba.html', time=datetime.now())
+# @app.route('/latinAmerica/cuba')
+# def latinAmericacuba():
+#     return render_template('latinAmerica/cuba.html', time=datetime.now())
 
 
-@app.route('/latinAmerica/dominica')
-def latinAmericadominica():
-    return render_template('latinAmerica/dominica.html', time=datetime.now())
+# @app.route('/latinAmerica/dominica')
+# def latinAmericadominica():
+#     return render_template('latinAmerica/dominica.html', time=datetime.now())
 
 
-@app.route('/latinAmerica/dominicanRepublic')
-def latinAmericadominicanRepublic():
-    return render_template('latinAmerica/dominicanRepublic.html', time=datetime.now())
+# @app.route('/latinAmerica/dominicanRepublic')
+# def latinAmericadominicanRepublic():
+#     return render_template('latinAmerica/dominicanRepublic.html', time=datetime.now())
 
 
-@app.route('/latinAmerica/ecuador')
-def latinAmericaecuador():
-    return render_template('latinAmerica/ecuador.html', time=datetime.now())
+# @app.route('/latinAmerica/ecuador')
+# def latinAmericaecuador():
+#     return render_template('latinAmerica/ecuador.html', time=datetime.now())
 
 
-@app.route('/latinAmerica/elSalvador')
-def latinAmericaelSalvador():
-    return render_template('latinAmerica/elSalvador.html', time=datetime.now())
+# @app.route('/latinAmerica/elSalvador')
+# def latinAmericaelSalvador():
+#     return render_template('latinAmerica/elSalvador.html', time=datetime.now())
 
 
-@app.route('/latinAmerica/grenada')
-def latinAmericagrenada():
-    return render_template('latinAmerica/grenada.html', time=datetime.now())
+# @app.route('/latinAmerica/grenada')
+# def latinAmericagrenada():
+#     return render_template('latinAmerica/grenada.html', time=datetime.now())
 
 
-@app.route('/latinAmerica/guatemala')
-def latinAmericaguatemala():
-    return render_template('latinAmerica/guatemala.html', time=datetime.now())
+# @app.route('/latinAmerica/guatemala')
+# def latinAmericaguatemala():
+#     return render_template('latinAmerica/guatemala.html', time=datetime.now())
 
 
-@app.route('/latinAmerica/guyana')
-def latinAmericaguyana():
-    return render_template('latinAmerica/guyana.html', time=datetime.now())
+# @app.route('/latinAmerica/guyana')
+# def latinAmericaguyana():
+#     return render_template('latinAmerica/guyana.html', time=datetime.now())
 
 
-@app.route('/latinAmerica/haiti')
-def latinAmericahaiti():
-    return render_template('latinAmerica/haiti.html', time=datetime.now())
+# @app.route('/latinAmerica/haiti')
+# def latinAmericahaiti():
+#     return render_template('latinAmerica/haiti.html', time=datetime.now())
 
 
-@app.route('/latinAmerica/honduras')
-def latinAmericahonduras():
-    return render_template('latinAmerica/honduras.html', time=datetime.now())
+# @app.route('/latinAmerica/honduras')
+# def latinAmericahonduras():
+#     return render_template('latinAmerica/honduras.html', time=datetime.now())
 
 
-@app.route('/latinAmerica/jamaica')
-def latinAmericajamaica():
-    return render_template('latinAmerica/jamaica.html', time=datetime.now())
+# @app.route('/latinAmerica/jamaica')
+# def latinAmericajamaica():
+#     return render_template('latinAmerica/jamaica.html', time=datetime.now())
 
 
-@app.route('/latinAmerica/mexico')
-def latinAmericamexico():
-    return render_template('latinAmerica/mexico.html', time=datetime.now())
+# @app.route('/latinAmerica/mexico')
+# def latinAmericamexico():
+#     return render_template('latinAmerica/mexico.html', time=datetime.now())
 
 
-@app.route('/latinAmerica/nicaragua')
-def latinAmericanicaragua():
-    return render_template('latinAmerica/nicaragua.html', time=datetime.now())
+# @app.route('/latinAmerica/nicaragua')
+# def latinAmericanicaragua():
+#     return render_template('latinAmerica/nicaragua.html', time=datetime.now())
 
 
-@app.route('/latinAmerica/saintKitts&Nevis')
-def latinAmericasaintKittsAndNevis():
-    return render_template('latinAmerica/saintKitts&Nevis.html', time=datetime.now())
+# @app.route('/latinAmerica/saintKitts&Nevis')
+# def latinAmericasaintKittsAndNevis():
+#     return render_template('latinAmerica/saintKitts&Nevis.html', time=datetime.now())
 
 
-@app.route('/latinAmerica/saintLucia')
-def latinAmericasaintLucia():
-    return render_template('latinAmerica/saintLucia.html', time=datetime.now())
+# @app.route('/latinAmerica/saintLucia')
+# def latinAmericasaintLucia():
+#     return render_template('latinAmerica/saintLucia.html', time=datetime.now())
 
 
-@app.route('/latinAmerica/stVincent&Grenadines')
-def latinAmericastVincentAndGrenadines():
-    return render_template('latinAmerica/stVincent&Grenadines.html', time=datetime.now())
+# @app.route('/latinAmerica/stVincent&Grenadines')
+# def latinAmericastVincentAndGrenadines():
+#     return render_template('latinAmerica/stVincent&Grenadines.html', time=datetime.now())
 
 
-@app.route('/latinAmerica/suriname')
-def latinAmericasuriname():
-    return render_template('latinAmerica/suriname.html', time=datetime.now())
+# @app.route('/latinAmerica/suriname')
+# def latinAmericasuriname():
+#     return render_template('latinAmerica/suriname.html', time=datetime.now())
 
 
-@app.route('/latinAmerica/trinidadAndTobago')
-def latinAmericatrinidadAndTobago():
-    return render_template('latinAmerica/trinidadAndTobago.html', time=datetime.now())
+# @app.route('/latinAmerica/trinidadAndTobago')
+# def latinAmericatrinidadAndTobago():
+#     return render_template('latinAmerica/trinidadAndTobago.html', time=datetime.now())
 
 
-@app.route('/latinAmerica/panama')
-def latinAmericamexicopanama():
-    return render_template('latinAmerica/panama.html', time=datetime.now())
+# @app.route('/latinAmerica/panama')
+# def latinAmericamexicopanama():
+#     return render_template('latinAmerica/panama.html', time=datetime.now())
 
 
-@app.route('/latinAmerica/paraguay')
-def latinAmericaparaguay():
-    return render_template('latinAmerica/paraguay.html', time=datetime.now())
+# @app.route('/latinAmerica/paraguay')
+# def latinAmericaparaguay():
+#     return render_template('latinAmerica/paraguay.html', time=datetime.now())
 
 
-@app.route('/latinAmerica/peru')
-def latinAmericaperu():
-    return render_template('latinAmerica/peru.html', time=datetime.now())
+# @app.route('/latinAmerica/peru')
+# def latinAmericaperu():
+#     return render_template('latinAmerica/peru.html', time=datetime.now())
 
 
-@app.route('/latinAmerica/uruguay')
-def latinAmericauruguay():
-    return render_template('latinAmerica/uruguay.html', time=datetime.now())
+# @app.route('/latinAmerica/uruguay')
+# def latinAmericauruguay():
+#     return render_template('latinAmerica/uruguay.html', time=datetime.now())
 
 
-@app.route('/latinAmerica/venezuela')
-def latinAmericavenezuela():
-    return render_template('latinAmerica/venezuela.html', time=datetime.now())
+# @app.route('/latinAmerica/venezuela')
+# def latinAmericavenezuela():
+#     return render_template('latinAmerica/venezuela.html', time=datetime.now())
 
 
-# LATIN AMERICA DEPENDENCIES
+# # LATIN AMERICA DEPENDENCIES
 
-@app.route('/latinAmerica/anguilla')
-def latinAmericaanguilla():
-    return render_template('latinAmerica/anguilla.html', time=datetime.now())
+# @app.route('/latinAmerica/anguilla')
+# def latinAmericaanguilla():
+#     return render_template('latinAmerica/anguilla.html', time=datetime.now())
 
 
-@app.route('/latinAmerica/aruba')
-def latinAmericaaruba():
-    return render_template('latinAmerica/aruba.html', time=datetime.now())
+# @app.route('/latinAmerica/aruba')
+# def latinAmericaaruba():
+#     return render_template('latinAmerica/aruba.html', time=datetime.now())
 
 
-@app.route('/latinAmerica/britishVirginIslands')
-def latinAmericabritishVirginIslands():
-    return render_template('latinAmerica/britishVirginIslands.html', time=datetime.now())
+# @app.route('/latinAmerica/britishVirginIslands')
+# def latinAmericabritishVirginIslands():
+#     return render_template('latinAmerica/britishVirginIslands.html', time=datetime.now())
 
 
-@app.route('/latinAmerica/caribbeanNetherlands')
-def latinAmericacaribbeanNetherlands():
-    return render_template('latinAmerica/caribbeanNetherlands.html', time=datetime.now())
+# @app.route('/latinAmerica/caribbeanNetherlands')
+# def latinAmericacaribbeanNetherlands():
+#     return render_template('latinAmerica/caribbeanNetherlands.html', time=datetime.now())
 
 
-@app.route('/latinAmerica/caymanIslands')
-def latinAmericacaymanIslands():
-    return render_template('latinAmerica/puertoRico.html', time=datetime.now())
+# @app.route('/latinAmerica/caymanIslands')
+# def latinAmericacaymanIslands():
+#     return render_template('latinAmerica/puertoRico.html', time=datetime.now())
 
 
-@app.route('/latinAmerica/curacao')
-def latinAmericacuracao():
-    return render_template('latinAmerica/curacao.html', time=datetime.now())
+# @app.route('/latinAmerica/curacao')
+# def latinAmericacuracao():
+#     return render_template('latinAmerica/curacao.html', time=datetime.now())
 
 
-@app.route('/latinAmerica/falklandIslands')
-def latinAmericafalklandIslands():
-    return render_template('latinAmerica/falklandIslands.html', time=datetime.now())
+# @app.route('/latinAmerica/falklandIslands')
+# def latinAmericafalklandIslands():
+#     return render_template('latinAmerica/falklandIslands.html', time=datetime.now())
 
 
-@app.route('/latinAmerica/frenchGuiana')
-def latinAmericafrenchGuiana():
-    return render_template('latinAmerica/frenchGuiana.html', time=datetime.now())
+# @app.route('/latinAmerica/frenchGuiana')
+# def latinAmericafrenchGuiana():
+#     return render_template('latinAmerica/frenchGuiana.html', time=datetime.now())
 
 
-@app.route('/latinAmerica/guadeloupe')
-def latinAmericaguadelope():
-    return render_template('latinAmerica/guadelope.html', time=datetime.now())
+# @app.route('/latinAmerica/guadeloupe')
+# def latinAmericaguadelope():
+#     return render_template('latinAmerica/guadelope.html', time=datetime.now())
 
 
-@app.route('/latinAmerica/martinique')
-def latinAmericamartinique():
-    return render_template('latinAmerica/martinique.html', time=datetime.now())
+# @app.route('/latinAmerica/martinique')
+# def latinAmericamartinique():
+#     return render_template('latinAmerica/martinique.html', time=datetime.now())
 
 
-@app.route('/latinAmerica/montserrat')
-def latinAmericamontserrat():
-    return render_template('latinAmerica/montserrat.html', time=datetime.now())
+# @app.route('/latinAmerica/montserrat')
+# def latinAmericamontserrat():
+#     return render_template('latinAmerica/montserrat.html', time=datetime.now())
 
 
-@app.route('/latinAmerica/sintMaarten')
-def latinAmericasintMaarten():
-    return render_template('latinAmerica/sintMaarten.html', time=datetime.now())
+# @app.route('/latinAmerica/sintMaarten')
+# def latinAmericasintMaarten():
+#     return render_template('latinAmerica/sintMaarten.html', time=datetime.now())
 
 
-@app.route('/latinAmerica/turksAndCaicos')
-def latinAmericaturksAndCaicos():
-    return render_template('latinAmerica/turksAndCaicos.html', time=datetime.now())
+# @app.route('/latinAmerica/turksAndCaicos')
+# def latinAmericaturksAndCaicos():
+#     return render_template('latinAmerica/turksAndCaicos.html', time=datetime.now())
 
 
-@app.route('/latinAmerica/USVirginIslands')
-def latinAmericaUSVirginIslands():
-    return render_template('latinAmerica/USVirginIslands.html', time=datetime.now())
+# @app.route('/latinAmerica/USVirginIslands')
+# def latinAmericaUSVirginIslands():
+#     return render_template('latinAmerica/USVirginIslands.html', time=datetime.now())
 
 
-@app.route('/latinAmerica/puertoRico')
-def latinAmericapuertoRico():
-    return render_template('latinAmerica/puertoRico.html', time=datetime.now())
+# @app.route('/latinAmerica/puertoRico')
+# def latinAmericapuertoRico():
+#     return render_template('latinAmerica/puertoRico.html', time=datetime.now())
 
 
-# EUROPEAN COUNTRIES
-# https://www.worldometers.info/geography/how-many-countries-in-europe/
-# DEPENDENCIES IN EUROPE
+# # EUROPEAN COUNTRIES
+# # https://www.worldometers.info/geography/how-many-countries-in-europe/
+# # DEPENDENCIES IN EUROPE
 
-# AFRICAN COUNTIRES
-# https://www.worldometers.info/geography/how-many-countries-in-africa/
-# AFRICAN IN EUROPE
+# # AFRICAN COUNTIRES
+# # https://www.worldometers.info/geography/how-many-countries-in-africa/
+# # AFRICAN IN EUROPE
 
-# OCEANA COUNTRIES
-# https://www.worldometers.info/geography/how-many-countries-in-oceania/
-# DEPENDENCIES OCEANA
+# # OCEANA COUNTRIES
+# # https://www.worldometers.info/geography/how-many-countries-in-oceania/
+# # DEPENDENCIES OCEANA
 
-# NORTH AMERICA
-# https://www.worldometers.info/geography/how-many-countries-in-northern-america/
-# NORTH AMERICA DEPENDENCIES
+# # NORTH AMERICA
+# # https://www.worldometers.info/geography/how-many-countries-in-northern-america/
+# # NORTH AMERICA DEPENDENCIES
